8d7b89a7369b7522c1d9823bd11aa28219899a4f
Adam Connelly <adam.rpconnelly@gmail.com>
2022-03-09 17:11:18 +0000

    Fix env var names

--- spacelift.tf
+++ spacelift.tf
@@ -3,14 +3,14 @@ resource "spacelift_context" "k8s-testing" {
   name        = "EKS Cluster Config"
 }
 
-resource "spacelift_environment_variable" "ireland-kubeconfig" {
+resource "spacelift_environment_variable" "eks_cluster_name" {
   context_id = spacelift_context.k8s-testing.id
   name       = "EKS_CLUSTER_NAME"
   value      = local.cluster_name
   write_only = false
 }
 
-resource "spacelift_environment_variable" "ireland-kubeconfig" {
+resource "spacelift_environment_variable" "eks_region" {
   context_id = spacelift_context.k8s-testing.id
   name       = "EKS_REGION"
   value      = var.region
