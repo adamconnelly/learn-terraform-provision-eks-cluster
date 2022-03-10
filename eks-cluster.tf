17dc5c088a3ff1628994d0a70c371fdb6031b22e
Adam Connelly <adam.rpconnelly@gmail.com>
2022-03-10 12:06:08 +0000

    Grant admin access to my user and test role

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -1,3 +1,7 @@
+data "aws_iam_role" "admin" {
+  name = "test-admin"
+}
+
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
   version         = "17.24.0"
@@ -25,6 +29,22 @@ module "eks" {
       additional_userdata           = "echo foo bar"
       additional_security_group_ids = [aws_security_group.worker_group_mgmt_two.id]
       asg_desired_capacity          = 1
+    },
+  ]
+
+  map_roles = [
+    {
+      rolearn  = data.aws_iam_role.admin.arn
+      username = data.aws_iam_role.admin.id
+      groups   = ["system:masters"]
+    }
+  ]
+
+  map_users = [
+    {
+      userarn  = "arn:aws:iam::039653571618:user/adamc"
+      username = "adamc"
+      groups   = ["system:masters"]
     },
   ]
 }
