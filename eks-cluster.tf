68bc64ff636e0d0035b9046bdd2970ae8bc4593e
Adam Connelly <adam.rpconnelly@gmail.com>
2022-03-11 16:10:02 +0000

    Remove old nodes

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -11,30 +11,9 @@ module "eks" {
 
   vpc_id = module.vpc.vpc_id
 
-  workers_group_defaults = {
-    root_volume_type = "gp2"
-  }
-
-  worker_groups = [
-    {
-      name                          = "worker-group-1"
-      instance_type                 = "t2.small"
-      additional_userdata           = "echo foo bar"
-      additional_security_group_ids = [aws_security_group.worker_group_mgmt_one.id]
-      asg_desired_capacity          = 2
-    },
-    {
-      name                          = "worker-group-2"
-      instance_type                 = "t2.medium"
-      additional_userdata           = "echo foo bar"
-      additional_security_group_ids = [aws_security_group.worker_group_mgmt_two.id]
-      asg_desired_capacity          = 1
-    },
-  ]
-
   node_groups = {
     containerd = {
-      desired_capacity = 1
+      desired_capacity = 2
       min_capacity     = 1
       max_capacity     = 3
 
